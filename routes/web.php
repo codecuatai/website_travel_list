bb5e6f22a2beacfa84f5565305930542000c0f01
codecuatai <vtai2805@gmail.com>
2025-12-17 17:49:50 +0700

    layouts auth

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/auth', function () {
+    return view('layouts.auth');
+});
+Route::get('/register', function () {
+    return view('auth/register');
+});
